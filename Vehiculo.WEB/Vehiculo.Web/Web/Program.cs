92a7260189fa6ff9eb2b5c172abdf49db0a0cd8b
Wiyumiyu <mariana.torres.valverde0@gmail.com>
2026-03-18 14:48:22 -0600

    final

---
+++ Vehiculo.WEB/Vehiculo.Web/Web/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Reglas;
+using Abstracciones.Interfaces.Reglas;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddScoped<IConfiguracion, Configuracion>();
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
---
+++ Vehiculo.WEB/Vehiculo.Web/Web/wwwroot/js/site.js
@@ -0,0 +1,22 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+// Animación al hacer scroll
+document.addEventListener("DOMContentLoaded", function () {
+
+    const elements = document.querySelectorAll(".fade-in");
+
+    function mostrarElementos() {
+        elements.forEach(el => {
+            const top = el.getBoundingClientRect().top;
+
+            if (top < window.innerHeight - 50) {
+                el.classList.add("show");
+            }
+        });
+    }
+
+    window.addEventListener("scroll", mostrarElementos);
+    mostrarElementos();
+});
